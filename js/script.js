897520600fcc72f07f1f5d5768c5ebcfa798aeef
tomaszcividino <tomaszcividino@gmail.com>
2021-04-23 16:49:30 +0200

    Init

---
+++ js/script.js
@@ -0,0 +1,70 @@
+'use strict';
+
+
+
+// const titleClickHandler = function(event) {
+//     console.log("Link was clicked!");
+//     const activeLinks = document.querySelectorAll('articles');
+
+//     for(let activeLink of activeLinks) {
+//         const article = document.getElementById(id)
+//         const id = activeLink.getAttribute('href');
+//         console.log("Hej");
+//         article = document.getElementById(id)
+//         article.classList.add('active')
+//     }
+// }
+// const links = document.querySelectorAll('.titles a');
+// for(let link of links) {
+// link.addEventListener('click', titleClickHandler)
+// }
+
+// 1. COMMIT TASK RUNNER MODUŁ 6 STRONA 20 //
+//  2. COMMIT REMOVECLASSFROMALLATRICLECLINKS -->
+
+/* remove class 'active' from all article links */
+
+
+    // for(let activeArticle of activeArticles) {
+    // activeArticle.classList.remove('active')
+    // }
+
+    
+    /* add class 'active' to the clicked link */
+
+//     console.log('clickedElement:', clickedElement)
+//     clickedElement.classList.add('active')
+//
+
+    /* remove class 'active' from all articles */
+
+    //     for(let activeLink of activeLinks) {
+//         console.log("Hej");
+//         activeLink.classList.remove('active');
+//     }
+// }
+
+/* find the correct article using the selector (value of 'href' attribute) */
+ /* add class 'active' to the correct article */
+
+
+    /* get 'href' attribute from the clicked link */
+
+
+
+    // const links = document.querySelectorAll('.titles a');
+    // const activeLinks = document.querySelectorAll('article');
+
+
+    // const titleClickHandler = function(event) {
+    //     console.log("Link was clicked!");
+    //     for(let activeLink of activeLinks) {
+    //     document.querySelector(id)
+    //         console.log("Hej");
+    //     }}
+    //     for(let link of links) {
+    //     link.addEventListener('click', titleClickHandler)
+    //     link.getAttribute('href');
+    //     }        
+
+
