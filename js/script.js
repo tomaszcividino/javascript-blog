bed2e3acb002d063329f5f0d1542a061aa5b563d
tomaszcividino <tomaszcividino@gmail.com>
2021-04-26 14:31:53 +0200

    JavaScript add Scrypt

--- js/script.js
+++ js/script.js
@@ -1,22 +1,38 @@
 'use strict';
 
-const activeLinks = document.querySelectorAll('.titles a');
-const articles = document.querySelectorAll('article');
+const titles = document.querySelectorAll('.titles a');
+const articles1 = document.querySelectorAll('article');
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
 const titleClickHandler = function(event) {
     const clickedElement = this;
-    for(let article of articles) {
+    for(let article of articles1) {
         article.classList.remove('active');
     }
-    for(let activeLink of activeLinks) {
-        activeLink.classList.remove('active');
+    for(let title of titles) {
+        title.classList.remove('active');
     }
     clickedElement.classList.add('active');
     const id = clickedElement.getAttribute('href');
     const activeArticle = document.querySelector(id);
-    activeArticle.classList.add('active')
+    activeArticle.classList.add('active');  
 }
-for(let activeLink of activeLinks) {
-    activeLink.addEventListener('click', titleClickHandler);
-}    
+function generateTitleLinks() {
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = "";
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
+    for(let article of articles) {
+        const articleId = article.getAttribute('id');
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+        titleList.innerHTML = titleList.innerHTML + linkHTML;
+    }
+    const activeLinks = document.querySelectorAll('.titles a');
+    for(let activeLink of activeLinks) {
+        activeLink.addEventListener('click', titleClickHandler);
+    }    
+}
 
-
+generateTitleLinks()
